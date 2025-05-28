dfce745bbae6ef2c259fcfb7be2b1dd9224cfbbb
guge431 <chuanzhenhe58@gmail.com>
2025-05-28 17:55:34 +0800

    feat:不知道啥文件

---
+++ .wrangler/tmp/bundle-3d2rau/middleware-insertion-facade.js
@@ -0,0 +1,4 @@
+				import { __facade_registerInternal__ } from "D:\\nvm\\nvm\\v22.15.0\\node_global\\node_modules\\wrangler\\templates\\middleware\\loader-sw.ts";
+				import * as __MIDDLEWARE_0__ from "D:\\nvm\\nvm\\v22.15.0\\node_global\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "D:\\nvm\\nvm\\v22.15.0\\node_global\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+				__facade_registerInternal__([__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default])
